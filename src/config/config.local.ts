9ddbe4d5ade8a48ac8310e6afa355e045d1abe09
栖鸟闲榆 <59347549+coxine@users.noreply.github.com>
2024-08-13 18:56:56 +0800

    fix: config

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -1,4 +1,8 @@
 import { MidwayConfig } from '@midwayjs/core';
+import { Task } from '../entity/task.entity';
+import { Project } from '../entity/project.entity';
+import { User } from '../entity/user.entity';
+import { Comment } from '../entity/comment.entity';
 
 export default {
   secretKey: '26117e60433ab66101c7fbc70afa9e9577c6c00d783cf2a183de4c536a2139f9',
@@ -21,7 +25,12 @@ export default {
         database: 'kanban',
         synchronize: true,
         logging: false,
-        entities: ['**/*.entity.{j,t}s'],
+        entities: [
+          Task,
+          Project,
+          User,
+          Comment
+        ],
       },
     },
   },
