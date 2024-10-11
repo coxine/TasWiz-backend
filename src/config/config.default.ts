43ed5030ee8abb6854a7e4fa6651d77f1c914b6d
coxine <59347549+coxine@users.noreply.github.com>
2024-10-11 10:20:33 +0800

    remove sensitive items in config

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -5,8 +5,8 @@ import { User } from '../entity/user.entity';
 import { Comment } from '../entity/comment.entity';
 
 export default {
-  secretKey: '26117e60433ab66101c7fbc70afa9e9577c6c00d783cf2a183de4c536a2139f9',
-  keys: '1723087639021_822',
+  secretKey: '',
+  keys: '',
   koa: {
     port: 7001,
   },
@@ -18,11 +18,11 @@ export default {
     dataSource: {
       default: {
         type: 'mysql',
-        host: 'sh-cynosdbmysql-grp-ftk1jspo.sql.tencentcdb.com',
+        host: '',
         port: 27229,
-        username: 'Yuxianjiang_kanban',
-        password: 'Yxjkanban240813qaq',
-        database: 'kanban',
+        username: '',
+        password: '',
+        database: '',
         synchronize: true,
         logging: false,
         entities: [Task, Project, User, Comment],
